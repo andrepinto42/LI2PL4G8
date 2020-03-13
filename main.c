2a3c886eddba0b50968756b1391e1a1ca251beae
andrepinto42 <61984266+andrepinto42@users.noreply.github.com>
2020-03-13 19:01:32 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include "estrutura.h"
 #include "lógica.h"
+#include "CamadaDeInterface.h"
 #include <stdlib.h>
 #include <string.h>
 
 int main()
 {
 ESTADO *e = inicializar_estado();
+ interpretador(e);
 }
