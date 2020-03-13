7d4070ae6fe76abd296c44af32fdfe6621f0ca2c
SurViVorG <56766947+SurViVorG@users.noreply.github.com>
2020-03-13 18:29:52 +0000

    Add files via upload

---
+++ CamadaDeInterface.c
@@ -0,0 +1,15 @@
+
+void mostrar_tabuleiro(Estado *e){
+    int a,b;
+    for ( a=0 ; a<=7 ; a++){
+        for ( b=0 ; b<=7 ; b++){
+            if ( a == 0 && b == 0 )
+                printf("1");
+            else if ( a == 5 && b == 5 )
+                printf("*");
+            else if ( a== 7 && b == 7)
+                printf("2");
+            else printf(".");
+        }
+    }
+}
---
+++ CamadaDeInterface.h
@@ -0,0 +1,8 @@
+#ifndef UNTITLED2_LIBRARY_H
+#define UNTITLED2_LIBRARY_H
+
+void mostrar_tabuleiro(Estado *e){
+
+}
+
+#endif //UNTITLED2_LIBRARY_H
