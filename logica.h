1d4632b6c09f48dca1356c1843421d21eb72dfe7
andrepinto42 <61984266+andrepinto42@users.noreply.github.com>
2020-03-13 18:16:19 +0000

    Update logica.h

--- logica.h
+++ logica.h
@@ -1,1 +1,8 @@
-int jogar (ESTADO *estado, COORDENADA c)
+
+#ifndef RASTROS02_LOGICA_H
+#define RASTROS02_LOGICA_H
+
+int jogar (ESTADO *estado, COORDENADA c);
+int valido(ESTADO *estado, COORDENADA c);
+
+#endif //RASTROS02_LOGICA_H
