654b4a28949dba050fab1bbc3b6012ce100e334d
Claudia54 <62038076+Claudia54@users.noreply.github.com>
2020-03-13 17:38:42 +0000

    Add files via upload

---
+++ lógica.c
@@ -0,0 +1,11 @@
+int jogar (ESTADO *estado, COORDENADA c){
+    printf ("jogar %d %d\n",c.coluna ,c.linha);
+
+    if ( estado ->tab[c.coluna][c.linha] = valido  (estado,[c.coluna][c.linha]){
+     set_Branca_Tabuleiro(estado, [c.coluna][c.linha]);
+    return 1;
+    else return 0;
+}
+valido (ESTADO *e , COORDENADAS c)) {
+    return 1 ;
+}
