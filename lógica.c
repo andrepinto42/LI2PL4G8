4e16a13cebf6138f79979c6f0fe0c9bc82dec00e
andrepinto42 <61984266+andrepinto42@users.noreply.github.com>
2020-03-13 17:49:00 +0000

    update alguns erros

--- lógica.c
+++ lógica.c
@@ -1,11 +1,14 @@
 int jogar (ESTADO *estado, COORDENADA c){
     printf ("jogar %d %d\n",c.coluna ,c.linha);
 
-    if ( estado ->tab[c.coluna][c.linha] = valido  (estado,[c.coluna][c.linha]){
-     set_Branca_Tabuleiro(estado, [c.coluna][c.linha]);
+    if (valido(estado,c))
+    {
+     set_Branca_Tabuleiro(estado,c);
     return 1;
+    }
     else return 0;
 }
-valido (ESTADO *e , COORDENADAS c)) {
+valido (ESTADO *e , COORDENADAS c) 
+{
     return 1 ;
 }
