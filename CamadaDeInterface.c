28512ffdaf8a08c6be2eeec5cd80c6048f3c5f9e
SurViVorG <56766947+SurViVorG@users.noreply.github.com>
2020-03-13 18:42:42 +0000

    Add files via upload

--- CamadaDeInterface.c
+++ CamadaDeInterface.c
@@ -1,6 +1,4 @@
-#include "estrutura.h"
-#include <stdio.h>
-
+// Função que imprime o tabuleiro
 void mostrar_tabuleiro(ESTADO *e){
     int a,b;
     for ( a=0 ; a<=7 ; a++){
@@ -15,3 +13,19 @@ void mostrar_tabuleiro(ESTADO *e){
         }
     }
 }
+//Interpretador fornecido pelo guião
+int interpretador(ESTADO *e) {
+    char linha[BUF_SIZE];
+    char col[2], lin[2];
+
+    if(fgets(linha, BUF_SIZE, stdin) == NULL)
+        return 0;
+
+    if(strlen(linha) == 3 && sscanf(linha, "%[a-h]%[1-8]", col, lin) == 2) {
+        COORDENADA coord = {*col - 'a', *lin - '1'};
+        jogar(e, coord);
+        mostrar_tabuleiro(e);
+    }
+    return 1;
+}
+
