11bd6c9ec7b956c63d27d2a25a36c84d04cfc8b1
andrepinto42 <andrepinto.goncalves@gmail.com>
2020-03-20 12:16:56 +0000

    update prompt

--- lógica.c
+++ lógica.c
@@ -5,12 +5,22 @@ int valido(ESTADO *e , COORDENADA c)
 {
     return 1 ;
 }
-int jogar (ESTADO *estado, COORDENADA c){
-    printf ("jogar %d %d\n",c.coluna ,c.linha);
-
+int jogar (ESTADO *estado, COORDENADA c)
+{
+    printf ("jogar %d %d\n",c.coluna ,7-(c.linha));
     if (valido(estado,c))
     {
      set_Branca_Tabuleiro(estado,c);
+     int jog = obter_jogador_atual(estado);
+     if (jog ==1)
+     {
+         set_numero_de_jogadas(estado);
+         set_jogador_atual(estado,2);
+     }
+     else
+     {
+         set_jogador_atual(estado,1);
+     }
     return 1;
     }
     else return 0;
