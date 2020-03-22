5f8fe6d6f7e4a79a4e516e1f7eab862e951a5611
andrepinto42 <61984266+andrepinto42@users.noreply.github.com>
2020-03-22 19:18:39 +0000

    update buggs

--- lógica.c
+++ lógica.c
@@ -1,119 +1,266 @@
 #include "estrutura.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
 
-int valido(ESTADO *e , COORDENADA c)
+int check_lado_oeste(ESTADO *e,COORDENADA c)// testa se tem pecas pretas na esquerda;
+{
+    int jL = c.linha;
+    int jC = c.coluna;
+    int offset= -1;
+
+    if (jC == 0) return 1; // Não pode ir para a esquerda;
+    else if (jL == 0)
+    {
+        if (obter_estado_casa(e,jL,jC-1) == PRETA ||
+            obter_estado_casa(e,jL+1,jC -1) == PRETA)
+        {
+            return 1;
+        }
+    }
+    else if (jL == 7)
+    {
+        if (obter_estado_casa(e,jL,jC-1) == PRETA ||
+            obter_estado_casa(e,jL-1,jC- 1) == PRETA)
+        {
+            return 1;
+        }
+    }
+    else
+    {
+        for (offset ; offset <=1 ; ++offset)
+        {
+            if (!(obter_estado_casa(e,jL+offset,jC-1) == PRETA))
+                break;
+        }
+    }
+    if (offset == 2) return 1;
+    else return 0;
+}
+
+
+int check_lado_este(ESTADO *e,COORDENADA c)// testa se tem pecas pretas na direita;
+{
+    int jL = c.linha;
+    int jC = c.coluna;
+    int offset= -1;
+
+    if (jC == 7) return 1; // Não pode ir para a direita;
+    else if (jL == 0)
+    {
+        if (obter_estado_casa(e,jL,jC+1) == PRETA ||
+            obter_estado_casa(e,jL+1,jC +1) == PRETA)
+        {
+            return 1;
+        }
+    }
+    else if (jL == 7)
+    {
+        if (obter_estado_casa(e,jL,jC+1) == PRETA ||
+            obter_estado_casa(e,jL-1,jC + 1) == PRETA)
+        {
+            return 1;
+        }
+    }
+    else
+    {
+        for (offset ; offset <=1 ; ++offset)
+        {
+            if (!(obter_estado_casa(e,jL+offset,jC+1) == PRETA))
+                break;
+        }
+    }
+    if (offset == 2) return 1;
+    else return 0;
+}
+
+
+int check_lado_sul(ESTADO *e,COORDENADA c)// testa se tem pecas pretas embaixo;
+{
+    int jL = c.linha;
+    int jC = c.coluna;
+    int offset= -1;
+
+    if (jL == 7) return 1; // Não pode ir para baixo;
+    else if (jC == 0)
+    {
+        if (obter_estado_casa(e,jL-1,jC) == PRETA ||
+            obter_estado_casa(e,jL-1,jC +1) == PRETA)
+        {
+            return 1;
+        }
+    }
+    else if (jC == 7)
+    {
+        if (obter_estado_casa(e,jL-1,jC) == PRETA ||
+            obter_estado_casa(e,jL-1,jC - 1) == PRETA)
+        {
+            return 1;
+        }
+    }
+    else
+    {
+        for (offset ; offset <=1 ; ++offset)
+        {
+            if (!(obter_estado_casa(e,jL-1,jC+offset) == PRETA))
+                break;
+        }
+    }
+    if (offset == 2) return 1;
+    else return 0;
+}
+
+
+int check_lado_norte(ESTADO *e,COORDENADA c)// testa se tem pecas pretas em cima;
+{
+    int jL = c.linha;
+    int jC = c.coluna;
+    int offset= -1;
+
+    if (jL == 0) return 1; // Não pode ir para cima;
+    else if (jC == 0)
+    {
+        if (obter_estado_casa(e,jL+1,jC) == PRETA ||
+            obter_estado_casa(e,jL+1,jC +1) == PRETA)
+        {
+            return 1;
+        }
+    }
+    else if (jC == 7)
+    {
+        if (obter_estado_casa(e,jL+1,jC) == PRETA ||
+            obter_estado_casa(e,jL+1,jC - 1) == PRETA)
+        {
+            return 1;
+        }
+    }
+    else
+        {
+            for (offset ; offset <=1 ; ++offset)
+            {
+             if (!(obter_estado_casa(e,jL+1,jC+offset) == PRETA))
+                break;
+            }
+    }
+    if (offset == 2) return 1;
+    else return 0;
+}
+
+
+
+ERROS valido(ESTADO *e , COORDENADA c)
 {
     int distlinha = pow(c.linha - obter_pos_jogador(e).linha,2);
     int distcoluna = pow(c.coluna - obter_pos_jogador(e).coluna,2);
 
     float dist = (sqrt( distlinha+ distcoluna ) ) ;
 
-        if ( obter_estado_casa(e,c.linha,c.coluna) == VAZIO )
+        if (dist < 1.42 )
         {
-            if (dist < 1.42 )
-            return 1 ;
+            if ( obter_estado_casa(e,c.linha,c.coluna) == VAZIO )
+            return OK ;
+            else
+                return COORDENADA_OCUPADA;
 
         }
-    return 0 ;
+        else return COORDENADA_INVALIDA;
+
+}
+/*
+int fim (ESTADO *e, COORDENADA c)
+{
+    {
+        if ( c.coluna==0 && c.linha ==8 )
+            return 3;
+        if (c.coluna ==8 && c.linha ==0)
+            return 2;
+        if ( e-> tab[c.linha][c.coluna+1] ==PRETA && e-> tab[c.linha-1][c.coluna+1]==PRETA
+             && e-> tab[c.linha-1][c.coluna-1] == PRETA && e-> tab[c.linha-1][c.coluna]==PRETA &&
+        e-> tab[c.linha-1][c.coluna-1] == PRETA &&e-> tab[c.linha][c.coluna-1]==PRETA &&
+        e-> tab[c.linha+1][c.coluna]==PRETA && e-> tab[c.linha+1][c.coluna+1]==PRETA &&
+        e-> tab[c.linha][c.coluna]==BRANCA )
+        {
+            return 4;
+        }
+        if (( e-> tab[c.linha][8]==BRANCA && e-> tab[c.linha-1][8]==PRETA
+              && e-> tab[c.linha-1][c.coluna-1] == PRETA && e-> tab[c.linha][c.coluna-1]==PRETA &&
+              e-> tab[c.linha+1][8]==PRETA && e-> tab[c.linha+1][c.coluna-1]==PRETA) ||
+            (e-> tab[0][c.coluna-1]==PRETA && e-> tab[0][c.coluna+1] == PRETA &&
+             e-> tab[0][c.coluna]==BRANCA && e-> tab[c.linha+1][c.coluna]==PRETA &&
+             e-> tab[c.linha+1][c.coluna-1]==PRETA && e-> tab[c.linha+1][c.coluna+1]==PRETA) ||
+
+            ( e-> tab[c.linha][0] == BRANCA  && e-> tab[c.linha+1][0]==PRETA
+              && e-> tab[c.linha-1][c.coluna+1] == PRETA && e-> tab[c.linha][c.coluna+1]==PRETA &&
+              e-> tab[c.linha-1][0]==PRETA && e-> tab[c.linha+1][c.coluna+1]==PRETA)||
+
+            ( e-> tab[8][c.coluna-1]==PRETA && e-> tab[8][c.coluna+1] == PRETA &&
+              e-> tab[8][c.coluna]==BRANCA && e-> tab[c.linha-1][c.coluna+1]==PRETA &&
+              e-> tab[c.linha-1][c.coluna-1]==PRETA &&  e->tab[c.linha-1][c.coluna]==PRETA) ||
+
+            (e->tab[0][0]==BRANCA && e->tab[0][1]==PRETA && e->tab[1][0]==PRETA && e->tab[1][1]==PRETA) ||
+            (e->tab[8][8]==BRANCA && e->tab[7][8]==PRETA && e->tab[7][7]==PRETA && e->tab[8][7]==PRETA ))
+
+            return 4 ;
+    }
+
+    return 1 ;
 }
 
-int verificar_enc_meio (ESTADO *e, COORDENADAS c){
- if (obter_estado_casa (e, linha+1,coluna) ==PRETA && obter_estado_casa (e,c.linha-1,c.coluna+1)==PRETA 
-    && obter_estado_casa (e,c.linha-1 ,c.coluna-1) == PRETA && obter_estado_casa (e,c.linha-1,c.coluna)==PRETA &&
-    && obter_estado_casa (e,c.linha ,c.coluna-1)==PRETA && obter_estado_casa (e,c.linha+1 ,c.coluna-1)==PRETA &&
-     obter_estado_casa (e,c.linha+1 ,c.coluna+1)==PRETA && obter_estado_casa (e,c.linha,c.coluna+1) &&
-      obter_estado_casa (e,c.linha,c.coluna]==BRANCA ){
-        return 1;  
-      }
+*/
 
-   int verificar_enc_ladod (ESTADO *e, COORDENADAS c){
-      if obter_estado_casa (e,c.linha ,8)==BRANCA && obter_estado_casa (e,c.linha-1,8)==PRETA
-    && obter_estado_casa(e,c.linha-1,c.coluna-1) == PRETA && obter_estado_casa(e,c.linha,c.coluna-1)==PRETA 
-    && obter_estado_casa (e,c.linha+1,8)==PRETA && obter_estado_casa (e,c.linha+1,c.coluna-1)==PRETA) {
-     return 1 ;
-   }  
-   }  
-
-   int verificar_enc_cima (ESTADO *e, COORDENADAS c){
-    if (obter_estado_casa(e,8 ,c.coluna-1)==PRETA && obter_estado_casa(e,8 ,c.coluna+1)== PRETA &&
-    obter_estado_casa(e,8,c.coluna)==BRANCA && obter_estado_casa(e,c.linha-1 ,c.coluna)==PRETA &&
-    obter_estado_casa(e,c.linha-1,c.coluna-1)==PRETA && obter_estado_casa (e,c.linha-1 ,c.coluna+1)==PRETA) {
-        return 1;
-    }
-   }
-
-   int verificar_enc_ladoe (ESTADO *e, COORDENADAS c){
-       if (obter_estado_casa(e,c.linha ,1) == BRANCA  && obter_estado_casa(e,c.linha+1,1)==PRETA 
-    && obter_estado_casa(e,c.linha-1,c.coluna+1) == PRETA && obter_estado_casa(e,c.linha,c.coluna+1)==PRETA &&
-    obter_estado_casa(e,c.linha-1,1)==PRETA && obter_estado_casa(e,c.linha+1 ,c.coluna+1)==PRETA){
-        return 1;
-    }
-   }
-
-   int verificar_enc_baixo (ESTADO *e, COORDENADAS c){
-    if (obter_estado_casa(e,1,c.coluna-1)==PRETA && obter_estado_casa(e,1,c.coluna+1) == PRETA &&
-    obter_estado_casa(e,1,c.coluna)==BRANCA && obter_estado_casa(e,c.linha+1,c.coluna+1)==PRETA &&
-    obter_estado_casa(e,c.linha+1,c.coluna-1)==PRETA && obter_estado_casa(e,c.linha+1,c.coluna)==PRETA){
-        return 1;
-    }
-   }
-
-   int verificar_enc_cantos (ESTADO *e, COORDENADAS c){
-    (if (obter_estado_casa(e,1,8)==BRANCA) &&(obter_estado_casa(e,1,7)==PRETA && (obter_estado_casa(e,2,7)==PRETA )&& (obter_estado_casa(e,2,8)==PRETA) ||
-     (obter_estado_casa(e,8,1)==BRANCA) &&(obter_estado_casa(e,7,1)==PRETA && (obter_estado_casa(e,7,2)==PRETA )&& (obter_estado_casa(e,8,2)==PRETA) ||
-            
-
-
-int fim (ESTADO *e, COORDENADAS c){
-    if ( c.coluna=0 && c.linha =8 ){
-    return 3;
-    }
-    if (c.coluna =8 && c.linha =0){
-    return 2;
-    }
-    if (verificar_enc_meio (e,c)==1){
-  return 4;
-    }
-    if (verificar_enc_ladod (e,c)==1){
-    return 4;
-    }
-    if (verificar_enc_cima (e,c) ==1){
-        return 4;
-    }
-    if (verificar_enc_ladoe (e,c)==1){
-        return 4;
-    }
-    if (verificar_enc_baixo (e,c)==1) {
-        return 4 ; 
-    }
-    if (verificar_enc_cantos (e,c) ==1 ){
-        return 4;
-    }
-
-
-
-
-  ERROS jogar (ESTADO *estado, COORDENADA c)
+ERROS jogar (ESTADO *estado, COORDENADA c)
 {
-    printf("jogar %d %d\n", c.coluna, 7-c.linha);
-    COORDENADA posicao_atual =obter_pos_jogador(estado);
-    if (valido(estado, c))
+
+    printf("jogar %d %d\n", c.coluna, 7 - c.linha);
+    COORDENADA posicao_atual = obter_pos_jogador(estado);
+    ERROS erro = valido(estado, c);
+    if (erro == OK)
     {
-        set_Branca_Tabuleiro(estado,c);
-        set_Preta_Tabuleiro(estado,posicao_atual);
-        int jog = obter_jogador_atual(estado);
-        if (jog ==1)
+        if (c.coluna == 7 && c.linha == 0)
         {
-            set_numero_de_jogadas(estado);
             set_jogador_atual(estado,2);
+            return ACABOU;
         }
-        else
+        else if (c.coluna == 0 && c.linha == 7)
         {
             set_jogador_atual(estado,1);
+            return ACABOU;
         }
-        return OK;
+        set_Branca_Tabuleiro(estado, c);
+        set_Preta_Tabuleiro(estado, posicao_atual);
+        int jog = obter_jogador_atual(estado);
+        if (jog == 1)
+        {
+            set_numero_de_jogadas(estado);
+            set_jogador_atual(estado, 2);
+        } else
+            {
+            set_jogador_atual(estado, 1);
+        }
+        return erro;
     }
+    else if (erro == COORDENADA_INVALIDA)
+    {
+        return erro;
+    } else if (check_lado_este(estado, c) && check_lado_norte(estado, c) &&
+               check_lado_oeste(estado, c) && check_lado_sul(estado, c))
+        // o jogador nao pode sair;
+    {
+        int jog = obter_jogador_atual(estado);
+        if (jog == 1)
+            set_jogador_atual(estado, 2);
+        else
+            set_jogador_atual(estado, 1);
+        return ACABOU;
+    }
+
+    else return erro;
+}
+
+
+/*
     if (fim(estado, c) == 1)
     {
 
@@ -132,4 +279,5 @@ int fim (ESTADO *e, COORDENADAS c){
         printf("Ganhou o jogador %c.Parabéns!\n", vencedor);
     }
 }
+*/
 
