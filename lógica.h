57081b7d9b71bca4303f208449a0258de91a4e29
andrepinto42 <61984266+andrepinto42@users.noreply.github.com>
2020-03-22 19:19:04 +0000

    Update lógica.h

--- lógica.h
+++ lógica.h
@@ -5,4 +5,9 @@
 int jogar (ESTADO *estado, COORDENADA c);
 int valido(ESTADO *estado, COORDENADA c);
 
+int check_lado_oeste(ESTADO *e,COORDENADA c);
+int check_lado_este(ESTADO *e,COORDENADA c);
+int check_lado_norte(ESTADO *e,COORDENADA c);
+int check_lado_sul(ESTADO *e,COORDENADA c);
+
 #endif //RASTROS02_LOGICA_H
